8d25a667ce91fa9eca5c94f0485245af05944d14
Tony <chidubemigwe449@gmail.com>
2026-04-23 14:10:30 +0100

    fix: mobile dropdown and navbar fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleNavClick = () => setIsOpen(false);
+
   return (
     <div className="navbar-container">
       <nav className="navbar">
@@ -7,30 +14,16 @@ function Navbar() {
             <a href="#hero">Chidubem T. Igwe</a>
           </div>
 
-          <ul className="links">
-            <li>
-              <a href="#hero" className="nav-link">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#about" className="nav-link">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#projects" className="nav-link">
-                Projects
-              </a>
-            </li>
-            <li>
-              <a href="#contacts" className="nav-link">
-                Contacts
-              </a>
-            </li>
+          <ul className={`links ${isOpen ? "links--open" : ""}`}>
+            <li><a href="#hero" className="nav-link" onClick={handleNavClick}>Home</a></li>
+            <li><a href="#about" className="nav-link" onClick={handleNavClick}>About</a></li>
+            <li><a href="#projects" className="nav-link" onClick={handleNavClick}>Projects</a></li>
+            <li><a href="#contacts" className="nav-link" onClick={handleNavClick}>Contacts</a></li>
           </ul>
 
-          <div className="menu">Menu</div>
+          <div className="menu" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <HiX /> : <HiMenu />}
+          </div>
         </div>
       </nav>
     </div>
